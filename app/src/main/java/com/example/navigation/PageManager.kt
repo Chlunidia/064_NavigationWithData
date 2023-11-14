27a020497ab9c7e68f03570cc269f2106aef20ec
Chlunidia Putri Kieswendha <kieswendha@gmail.com>
2023-11-14 21:49:41 +0700

    Rename function from SecondPage to SummaryPage, add new navigation, rename function cancelOrderAndNavigateToHome to cancelOrderAndNavigateToContact , and create function cancelContactAndNavigateToHome

--- app/src/main/java/com/example/navigation/PageManager.kt
+++ app/src/main/java/com/example/navigation/PageManager.kt
@@ -27,11 +27,13 @@ import androidx.navigation.compose.rememberNavController
 import com.example.navigation.data.DataSource.flavors
 
 enum class PageManager {
-    Home,
-    Flavor,
-    Summary
+    HomePage,
+    ContactPage,
+    FlavorPage,
+    SummaryPage
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun IceTeaAppBar(
     backNavigation: Boolean,
@@ -72,27 +74,36 @@ fun IceTeaApp(
         val uiState by viewModel.stateUI.collectAsState()
         NavHost(
             navController = navController,
-            startDestination = PageManager.Home.name,
+            startDestination = PageManager.HomePage.name,
             modifier = Modifier.padding(innerPadding)
         ) {
-            composable(route = PageManager.Home.name) {
+            composable(route = PageManager.HomePage.name) {
                 HomePage(
                     onNextButtonClicked = {
-                        navController.navigate(PageManager.Flavor.name)
+                        navController.navigate(PageManager.ContactPage.name)
                     }
                 )
             }
-            composable(route = PageManager.Flavor.name) {
+            composable(route = PageManager.ContactPage.name) {
+                ContactPage(
+                    onSubmitButtonClicked = { orderUIState ->
+                        viewModel.setContact(orderUIState)
+                        navController.navigate(PageManager.FlavorPage.name)
+                    },
+                    onCancelButtonClicked = { cancelContactAndNavigateToHome(viewModel, navController) }
+                )
+            }
+            composable(route = PageManager.FlavorPage.name) {
                 val context = LocalContext.current
-                FirstPage(
+                FlavorPage(
                     flavorChoice = flavors.map { id -> context.resources.getString(id) },
                     onSelectionChanged = {viewModel.setFlavor(it)},
                     onConfirmButtonClicked = {viewModel.setQuantity(it)},
-                    onNextButtonClicked = { navController.navigate(PageManager.Summary.name) },
-                    onCencelButtonClicked = { cancelOrderAndNavigateToHome(viewModel, navController) })
+                    onNextButtonClicked = { navController.navigate(PageManager.SummaryPage.name) },
+                    onCencelButtonClicked = { cancelOrderAndNavigateToContact(viewModel, navController) })
             }
-            composable(route = PageManager.Summary.name) {
-                SecondPage(
+            composable(route = PageManager.SummaryPage.name) {
+                SummaryPage(
                     orderUIState = uiState,
                     onCancelButtonClicked = { cancelOrderAndNavigateToFlavor(navController) }
                 )
@@ -101,11 +112,14 @@ fun IceTeaApp(
     }
 }
 
-fun cancelOrderAndNavigateToHome(viewModel: OrderViewModel, navController: NavHostController) {
+fun cancelOrderAndNavigateToContact(viewModel: OrderViewModel, navController: NavHostController) {
     viewModel.resetOrder()
-    navController.popBackStack(PageManager.Home.name, inclusive = false)
+    navController.popBackStack(PageManager.ContactPage.name, inclusive = false)
 }
-
+fun cancelContactAndNavigateToHome(viewModel: OrderViewModel, navController: NavHostController) {
+    viewModel.resetContact()
+    navController.popBackStack(PageManager.HomePage.name, inclusive = false)
+}
 fun cancelOrderAndNavigateToFlavor(navController: NavHostController) {
-    navController.popBackStack(PageManager.Flavor.name, inclusive = false)
+    navController.popBackStack(PageManager.FlavorPage.name, inclusive = false)
 }
