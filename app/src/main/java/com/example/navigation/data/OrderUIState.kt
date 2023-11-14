96085ca97665ec6db79b1df18601ba45f0cb43e6
Chlunidia Putri Kieswendha <kieswendha@gmail.com>
2023-11-14 15:49:54 +0700

    Add new variable to data class OrderUIState

--- app/src/main/java/com/example/navigation/data/OrderUIState.kt
+++ app/src/main/java/com/example/navigation/data/OrderUIState.kt
@@ -1,6 +1,9 @@
 package com.example.navigation.data
 
 data class OrderUIState(
+    val name: String = "",
+    val address: String = "",
+    val tlp: String = "",
     val quantity: Int = 0,
     val flavor: String = "",
     val price: String = "",
